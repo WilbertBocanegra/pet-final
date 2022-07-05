4dc2816792d76457bb23b934cea6d71eb0abbd13
Wilbert Bocanegra <wilbertbocanegra@gmail.com>
2022-07-05 17:15:26 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+
+  /**
+   * @type {HTMLIFrameElement}
+   */
+  let component;
+
+  let isLogin = false;
+
+  onMount(() => {
+    console.log("hola", component.contentDocument.cookie);
+  });
+</script>
+
+<iframe
+  bind:this={component}
+  src="https://final-login.vercel.app/"
+  title={undefined}
+/>
+
+<h1>PET</h1>
+<code>{isLogin}</code>
